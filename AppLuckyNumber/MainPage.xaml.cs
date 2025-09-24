63227fe17e5841543920cf63ef3893e1782f4ea5
Yago S Santos <yago@fantury.com.br>
2025-09-24 15:40:55 -0300

    feat: ALN Criação de MainPage e Code-behind

--- AppLuckyNumber/MainPage.xaml.cs
+++ AppLuckyNumber/MainPage.xaml.cs
@@ -1,24 +1,39 @@
-﻿namespace AppLuckyNumber
+namespace AppLuckyNumber;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnGenerateLuckNumbers(object sender, EventArgs e)
     {
-        int count = 0;
+        NameApp.IsVisible = false;
+        ContainerLuckNumbers.IsVisible = true;
 
-        public MainPage()
-        {
-            InitializeComponent();
+        var set = GenerateLuckNumbers();
+
+        LuckNumber01.Text = set.ElementAt(0).ToString("D2");
+        LuckNumber02.Text = set.ElementAt(1).ToString("D2");
+        LuckNumber03.Text = set.ElementAt(2).ToString("D2");
+        LuckNumber04.Text = set.ElementAt(3).ToString("D2");
+        LuckNumber05.Text = set.ElementAt(4).ToString("D2");
+        LuckNumber06.Text = set.ElementAt(5).ToString("D2");        
+    }
+
+    private SortedSet<int> GenerateLuckNumbers()
+    {
+        var set = new SortedSet<int>();
+
+        while (set.Count < 6)
+        { 
+            var random = new Random();
+            var luckNumber = random.Next(1, 61);
+
+            set.Add(luckNumber);
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        return set;
     }
 }
