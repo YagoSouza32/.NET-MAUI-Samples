3adeda7c26d7aeffffec295c98b854917b1135c1
Yago S Santos <yago@fantury.com.br>
2025-11-03 08:53:29 -0300

    feat: Exemplos para StackLayout

---
+++ AppMAUIGallery/Repositories/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using AppMAUIGallery.Models;
+using AppMAUIGallery.Views.Layouts;
+
+namespace AppMAUIGallery.Repositories;
+
+public class CategoryRepository
+{
+    public CategoryRepository() { }
+
+    public List<Category> GetCategories()
+    {
+        List<Category> categories = new List<Category>();
+
+        categories.Add(new Category
+        {
+            Name= "Layout",
+            Components = new List<Component> { 
+                new Component
+                { 
+                    Title = "Stacklayout",
+                    Description="Organização sequêncial dos elementos",
+                    Page = typeof(StackLayoutPage)
+                },
+                new Component
+                { 
+                    Title = "Grid",
+                    Description="Organização os elementos dentro de uma tabela",
+                    Page = typeof(GridLayoutPage)
+                },
+            }            
+        });
+        return categories;
+    }
+}
